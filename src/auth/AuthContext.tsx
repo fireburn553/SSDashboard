f93a12b0b386300904c603dc9c8637aa330a82b7
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2025-10-01 18:07:19 +0800

    added disable for useAuth

--- src/auth/AuthContext.tsx
+++ src/auth/AuthContext.tsx
@@ -156,6 +156,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) throw new Error("useAuth must be used within AuthProvider");
