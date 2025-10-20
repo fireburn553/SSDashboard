397e0f53c9f6836dc01514545e1eb46d4fa6312e
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2025-10-20 22:19:41 +0800

    Update index.js

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DB_URL;
 
 console.log(`[DB INIT] Production mode: ${isProduction}`);
 if (!connectionString) {
