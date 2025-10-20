48e847d027d459a99b4636eaa18f4d1ff704e8ec
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2025-10-20 16:23:23 +0800

    Fix import path for SignIn component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import SignIn from "./auth/Signin";
+import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
 import InstructorHome from "./pages/Instructor/InstructorHome";
 import NotFound from "./pages/NotFound";
